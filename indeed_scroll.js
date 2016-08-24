a4495a3bca3c671e8a270c8dc2794e4ef6998418
Simon Parker <simonp99@googlemail.com>
2016-08-25 00:23:44 +0100

    fixes a logic error bug and makes smoother scrolling
    scrolls page by one pixel at a time when at the default speed. also
    fixed a bug where scrolling would not restart

--- indeed_scroll.js
+++ indeed_scroll.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://www.indeed.co.uk/*
+// @match        http://www.indeed.co.uk/jobs*
+// @match        http://www.indeed.co.uk/recommendedjobs*
 // @grant        none
 // ==/UserScript==
 
@@ -16,26 +17,43 @@ var _ss_interval_pointer="notstarted";
 var _ss_speed = 6;
 var running=false;
     var _ss_abs_speed,_ss_direction ,_ss_speed_pair;
-var _ss_speed_pairs = [
+var _ss_speed_pairs = [ /* scroll_amount, interval_ms */
 	[0, 0],
 	[1, 200.0],
 	[1, 120.0],
 	[1, 72.0],
 	[1, 43.2],
 	[1, 25.9],
-	[2, 31.0],
-	[4, 37.2],
+	//[2, 31.0],
+	[1, 10],
+    [4, 37.2],
 	[8, 44.8],
 	[8, 26.4],
 	[16, 32.0]
 ];
 var _ss_last_onkeypress = document.onkeypress;
+var paused=false;
 var _ss_stop = function() {
 	clearTimeout(_ss_interval_pointer);
     running=false;
     setCookie("running", "false", 365);
 };
-var _ss_start = function() {
+        function onScrollEventHandler(ev)
+    {
+        //alert(ev);
+        //http://dev.w3.org/2006/webapi/DOM-Level-3-Events/html/DOM3-Events.html#event-type-scroll
+        _ss_stop();paused=true;
+        setTimeout(function(){
+                                paused=false;
+                                _ss_start();
+                             }, 1500);
+    } 
+
+    var el=window;
+   // el.addEventListener('scroll', onScrollEventHandler, false);   
+ 
+    var _ss_start = function() {
+    //if (paused) return;
     if (_ss_interval_pointer!="notstarted") clearTimeout(_ss_interval_pointer);
     setCookie("running", "true", 365);
     console.log("start");running=true;
@@ -44,8 +62,12 @@ var _ss_start = function() {
 	 _ss_speed_pair = _ss_speed_pairs[_ss_abs_speed];
     
 	_ss_interval_pointer = setInterval(function () {
-        scrollBy(0,_ss_direction * _ss_speed_pair[0]); 
+      //  el.removeEventListener('scroll', onScrollEventHandler, false); console.log("removed event handler");
+        scrollBy(0,_ss_direction * _ss_speed_pair[0]);// console.log("scroll");
         if((pageYOffset<=1)||(pageYOffset==document.height-innerHeight)) _ss_speed=0;
+     //   el.addEventListener('scroll', onScrollEventHandler, false);   
+
+
       }, _ss_speed_pair[1]);
 };
 var _ss_adj = function(q) {
@@ -66,6 +88,7 @@ document.onkeypress = function(e) {
 		return;
 	};
     	if ((e.charCode == 223) ) {
+        _ss_speed =6;
 		_ss_start();
             
 		return;
